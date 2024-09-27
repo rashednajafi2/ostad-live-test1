6b751053a016e4f8a62c9fd3a74c9e3105946a15
Najafi <rashednajafi@yahoo.com>
2024-09-27 23:32:16 +0600

    Live Test 1 Final Code Final Push

--- lib/livetest1.dart
+++ lib/livetest1.dart
@@ -1,1 +1,28 @@
-//This is test comment
+//Live Test 1 Code Here
+
+//Car Class
+class Car {
+  String brand;
+  String model;
+  int year;
+
+//Constructor properties values
+  Car(this.brand, this.model, this.year);
+
+//Car age calculation
+  int carAge() {
+    int currentYear = DateTime.now().year;
+    return currentYear - year;
+  }
+}
+
+void main() {
+  Car myCar = Car("Toyota", "Corolla", 2015);
+
+//Print Code
+  print("Brand: ${myCar.brand}");
+  print("Model: ${myCar.model}");
+  print("Year: ${myCar.year}");
+  print("Car Age: ${myCar.carAge()} years");
+
+}
